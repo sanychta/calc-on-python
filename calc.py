7a6d00863d5130d0975bddac8e3ca42a6be3e47b
sanychta <sanychta@yandex.ru>
2026-06-10 18:09:58 +0300

    Add input numbers and operation

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 def calc():
     print("=" * 30)
-    print("Калькулятор")
+    print("\t Калькулятор")
     print("=" * 30)
     print("Доступные операции:")
     print("  + : Сложение")
@@ -12,6 +12,25 @@ def calc():
     print("  q : Выход")
     print("-" * 30)
     
+    while True:
+        num1 = input("\nВведите первое число (или 'q' для выхода): ")
+        if num1.lower() == 'q':
+            print("До свидания!")
+            break
+            
+        num1 = float(num1)
+            
+        operation = input("Введите операцию (+, -, *, /, %, ^): ")
+        if operation.lower() == 'q':
+            print("До свидания!")
+            break
+            
+        num2 = input("Введите второе число: ")
+        if num2.lower() == 'q':
+            print("До свидания!")
+            break
+            
+            
 # Запуск калькулятора
 if __name__ == "__main__":
     calc()
